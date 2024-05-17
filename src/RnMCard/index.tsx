d407be0377b4027ff3874d02d55f358b5b0aa005
Olga <olginarom@gmail.com>
2024-05-17 14:35:35 +0200

    first_commit

---
+++ src/RnMCard/index.tsx
@@ -0,0 +1,11 @@
+export default function RnMCard() {
+  return (
+    <div>
+      <h1>Olga</h1>
+      <ul>
+        <li>Eta: 33 anni</li>
+        <li>Provenienza: Ucraina</li>
+      </ul>
+    </div>
+  );
+}
